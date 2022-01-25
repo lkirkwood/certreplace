1b7fcc66e70f4bcfab6b8d29cbac9952f57126ef
Linus Kirkwood <linuskirkwood@gmail.com>
2022-01-26 01:34:59 +1100

    fleshed out cli, moved to single specified path

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use structopt::StructOpt;
+use x509_parser::prelude::X509Certificate;
+use std::fmt::Error;
+use std::path::PathBuf;
 use std::{fs, io};
 use std::collections::HashSet;
 use log::warn;
 
-const TEST_CN: &str = "domain.com";
-
 #[derive(StructOpt)]
 struct Cli {
+    /// Path to search in.
+    path: String,
+    /// Common name to match in target certificates.
+    #[structopt(short = "cn")]
+    common_name: Option<String>,
     /// Path to public key to use as replacement.
-    public_key: String,
+    #[structopt(long = "cert")]
+    certificate: String,
     /// Path to private key to use as replacement.
+    #[structopt(long = "priv")]
     private_key: String
 }
 
@@ -24,127 +32,66 @@ struct CertBundle {
 
 fn main() {
     let args = Cli::from_args();
-    let public_key_path = args.public_key;
-    let private_key_path = args.private_key;
+    let search_path = args.path;
+    let new_cert_path = args.certificate;
+    let new_privkey_path = args.private_key;
 
-    let public_key = fs::read_to_string(public_key_path.clone())
-        .expect(format!("Failed to read file at {}", public_key_path).as_str());
+    let new_cert = fs::read_to_string(new_cert_path.clone())
+        .expect(format!("Failed to read file at {}", new_cert_path).as_str());
 
-    let private_key = fs::read_to_string(private_key_path.clone())
-        .expect(format!("Failed to read file at {}", private_key_path).as_str());
+    let new_privkey = fs::read_to_string(new_privkey_path.clone())
+        .expect(format!("Failed to read file at {}", new_privkey_path).as_str());
 
-    let common_name = cn_from_public_key(&public_key)
+    let common_name = match args.common_name {
+        Some(cn) => cn,
+        None => cn_from_public_key(&new_cert)
+            .expect("Failed to parse common name from public key.")
+    };
+    let common_name = cn_from_public_key(&new_cert)
         .expect("Failed to parse common name from public key.");
 
-    for certfile in search_for_certs(&common_name) {
-        replace_cert(&certfile.public_key_path, &public_key);
-        replace_cert(&certfile.private_key_path, &private_key);
+    let certs: Vec<X509Certificate> = Vec::new();
+    let privkeys: Vec<String> = Vec::new();
+    for path in match search_for_certs(&search_path) {
+        Err(err) => panic!("{}", err), 
+        Ok(paths) => paths
+    } {
+        // TODO implement parsing
     }
 }
 
 fn valid_cn(common_name: &String) -> bool {true}
 
-#[derive(PartialEq, Eq, Hash)]
-/// A public or private X.509 key
-struct Key {
-    /// The string content of the key.
-    value: String,
-    /// Common name from the key, if is_public.
-    common_name: Option<String>,
-    /// Whether this key is a public key.
-    is_public: bool
+fn cn_from_public_key(public_key: &String) -> Result<String, Error> {
+    Ok(String::from("domain.com"))
 }
 
-impl Key {
-    fn valid_pubkey(string: &String) -> bool {true} //TODO implement pub/privkey validation
-    
-    fn valid_privkey(string: &String) -> bool {true}
-
-    fn pubkey_from_string(string: &String) -> Result<Key, InvalidKey> {
-        cn_from_public_key(string).and_then(|cn| Ok(Key {
-            value: String::from("public key"),
-            common_name: Some(cn),
-            is_public: true
-        }))
-    }
-
-    fn privkey_from_string(string: &String) -> Result<Key, InvalidKey> {
-        Ok(Key {
-            value: String::from("private key"),
-            common_name: None,
-            is_public: false
-        })
-    }
-}
-
-#[derive(Debug, Clone)]
-/// A string which could not be parsed as a public or private X.509 key.
-struct InvalidKey {string: String}
-
-fn cn_from_public_key(public_key: &String) -> Result<String, InvalidKey> {Ok(String::from("domain.com"))}
-
-const SEARCH_LOCATIONS: [&str; 3] = [
-    "/etc/ssl/",
-    "/etc/nginx",
-    "/etc/httpd"
+const SEARCH_EXTENSIONS: [&str; 5] = [
+    "pem", "cer", "der", "crt", "key"
 ];
 
-fn search_for_certs(common_name: &String) -> Vec<CertBundle> {
-    let mut certs: Vec<CertBundle> = Vec::new();
-    let mut keys = KeySet{public_keys: HashSet::new(), private_keys: HashSet::new()};
-    for path in SEARCH_LOCATIONS.iter() {
-        for key_set in search_dir(path, common_name) {
-            keys.public_keys.extend(key_set.public_keys)
-        }
-    }
-    return certs
-}
-
-const SEARCH_EXTENSIONS: [&str; 0] = [
-
-];
-
-/// Set of X.509 certificate keys.
-struct KeySet {
-    /// Public X.509 keys
-    public_keys: HashSet<Key>,
-    /// Private X.509 keys
-    private_keys: HashSet<Key>
-}
-
-/// Recursively searches a directory for X.509 keys.
-fn search_dir(path: &str, common_name: &String) -> Result<KeySet, io::Error> {
-    let mut keys = KeySet {public_keys: HashSet::new(), private_keys: HashSet::new()};
+/// Recursively searches a directory for files with matching extensions. 
+/// Returns their paths.
+fn search_for_certs(path: &str) -> Result<HashSet<PathBuf>, io::Error> {
+    let mut paths = HashSet::new();
     for entry in fs::read_dir(&path)? {
         let entry = match entry {
-            Ok(entry) => entry,
-            Err(_) => continue
-        };
+            Err(_) => continue, Ok(val) => val};
         let entry_type = match entry.file_type() {
-            Ok(entry_type) => entry_type,
-            Err(_) => continue
-        };
+            Err(_) => continue, Ok(val) => val};
 
         if entry_type.is_file() {
-            let contents = match fs::read_to_string(entry.path()) {
-                Ok(contents) => contents,
-                Err(_) => continue
-            };
+            let path = entry.path();
+            let ext = match path.extension() {
+                None => continue, Some(val) => match val.to_str() {
+                    None => continue, Some(val) => val}};
+            if SEARCH_EXTENSIONS.contains(&ext) {
+                paths.insert(path);
+            }
 
-            match Key::pubkey_from_string(&contents) {
-                Ok(key) => match &key.common_name {
-                    Some(cn) => {if cn == common_name {
-                        keys.public_keys.insert(key);};},
-                    None => continue
-                }
-                Err(key) => match Key::privkey_from_string(&key.string) {
-                    Ok(key) => {keys.private_keys.insert(key);},
-                    Err(_) => continue
-                }
-            };
         }
     }
-    return Ok(keys);
+    return Ok(paths);
 }
 
 fn replace_cert(path: &str, content: &str) {
