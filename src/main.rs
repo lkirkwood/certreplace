189b89fdcfd0db0861b6fdbb1e2f1a6ee6f945ec
Linus Kirkwood <linuskirkwood@gmail.com>
2022-11-24 16:15:58 +1100

    Moved stucts into model and added verb

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,86 @@
+mod model;
+
 use log::{error, warn};
+use model::*;
 use openssl::nid::Nid;
 use openssl::pkey::{PKey, Private};
 use openssl::x509::X509;
 use std::fmt::Display;
 use std::path::PathBuf;
-use std::{fs, io};
+use std::{
+    fs,
+    io::{self, Write},
+};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-struct Cli {
+pub struct Cli {
     /// Path to search in.
-    path: String,
+    pub path: String,
     /// Common name to match in target certificates.
     #[structopt(short = "cn")]
-    common_name: Option<String>,
+    pub common_name: Option<String>,
     /// Path to public key to use as replacement.
     #[structopt(long = "cert")]
-    certificate: Option<String>,
+    pub certificate: Option<String>,
     /// Path to private key to use as replacement.
     #[structopt(long = "priv")]
-    private_key: Option<String>,
-}
-
-#[derive(Debug)]
-enum Verb {
-    Find,
-    Replace,
-}
-
-impl Display for Verb {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        return match self {
-            Self::Find => write!(f, "Finding"),
-            Self::Replace => write!(f, "Replacing"),
-        };
-    }
-}
-
-#[derive(Debug, Clone)]
-/// Models an X509 certificate.
-struct Cert {
-    cert: X509,
-    common_name: String,
-    path: PathBuf,
-}
-
-#[derive(Debug, Clone)]
-/// Models an X509 certificate private key.
-struct PrivKey {
-    key: PKey<Private>,
-    path: PathBuf,
-}
-
-#[derive(Debug)]
-/// Holds paths of all the objects to be replaced.
-struct ReplacePaths {
-    certs: Vec<PathBuf>,
-    keys: Vec<PathBuf>,
-}
-
-#[derive(Debug)]
-enum PKIObject {
-    Cert(Cert),
-    PrivKey(PrivKey),
+    pub private_key: Option<String>,
 }
 
 fn main() {
     let args = Cli::from_args();
 
-    let common_name = match args.common_name {
-        Some(cn) => cn,
-        None => {
-            if let Some(new_cert_path) = &args.certificate {
-                let new_cert = match fs::read(new_cert_path) {
-                    Err(err) => panic!("Failed to read file at {}: {:?}", new_cert_path, err),
-                    Ok(content) => match new_cert_path.rsplit_once('.') {
-                        None => {
-                            panic!("No file extension on new certificate to infer format from.")
-                        }
-                        Some((_, ext)) => match ext {
-                            "pem" | "crt" | "cer" => X509::from_pem(&content).unwrap(),
-                            other => todo!("Support extension {}", other),
-                        },
-                    },
-                };
-                get_cn(&new_cert).expect("Failed to get common name from provided certificate.")
-            } else {
-                panic!("No common name or replacement certificate provided.")
-            }
-        }
-    };
-
-    let verb = match args.certificate {
-        Some(_) => Verb::Replace,
-        None => Verb::Find,
-    };
-
-    if get_user_consent(&verb, &common_name, args.private_key.is_some()) {
-        let privkey = match args.private_key {
-            Some(file) => {
-                match parse_privkey(&fs::read(file).expect("Failed to read supplied private key."))
-                {
-                    Some(pkey) => Some(pkey),
-                    None => panic!("Failed to parse private key from provided file."),
+    let verb = match &args.certificate {
+        Some(cert_path) => {
+            let mut x509s = parse_cert(
+                &fs::read(&cert_path).expect("Failed to read provided private key file."),
+            );
+            let x509 = match x509s.len() {
+                1 => x509s.pop().unwrap(),
+                other => panic!("Wrong number of certs found in provided file: {}", other),
+            };
+            let cn = match get_cn(&x509) {
+                None => match args.common_name {
+            None => panic!("No common name provided and failed to parse one from the provided certificate.."),
+                    Some(cn) => cn,
                 }
-            }
-            None => None,
-        };
+                Some(cn) => cn
+            };
+            let cert = Cert {
+                cert: x509,
+                common_name: cn.clone(),
+                path: PathBuf::from(cert_path),
+            };
+
+            let privkey = match args.private_key {
+                None => None,
+                Some(privkey_path) => {
+                    let content =
+                        fs::read(&privkey_path).expect("Failed to read provided private key file.");
+                    Some(PrivKey {
+                        key: parse_privkey(&content).unwrap(),
+                        path: PathBuf::from(privkey_path),
+                    })
+                }
+            };
+            Verb::Replace { cn, cert, privkey }
+        }
+        None => match args.common_name {
+            None => panic!("No certificate or common name provided."),
+            Some(cn) => Verb::Find { cn },
+        },
+    };
+
+    if get_user_consent(&verb) {
         println!(
             "Matches: \n{:#?}",
-            find_certs(PathBuf::from(args.path), &common_name, privkey.is_some())
+            find_certs(PathBuf::from(args.path), verb.cn(), verb.privkeys())
         );
     } else {
         panic!(
             "User declined to replace objects for common name: {}",
-            common_name
+            verb.cn()
         );
     }
 }
@@ -132,6 +98,19 @@ fn parse_privkey(content: &[u8]) -> Option<PKey<Private>> {
     };
 }
 
+/// Parses a certificate from some bytes.
+fn parse_cert(content: &[u8]) -> Vec<X509> {
+    return if let Ok(cert) = X509::from_pem(&content) {
+        vec![cert]
+    } else if let Ok(cert) = X509::from_der(&content) {
+        vec![cert]
+    } else if let Ok(certs) = X509::stack_from_pem(&content) {
+        certs
+    } else {
+        vec![]
+    };
+}
+
 /// Gets the common name from a certificate if possible.
 fn get_cn(cert: &X509) -> Option<String> {
     if let Some(data) = cert.subject_name().entries_by_nid(Nid::COMMONNAME).next() {
@@ -143,18 +122,11 @@ fn get_cn(cert: &X509) -> Option<String> {
 }
 
 /// Returns true if user confirms operation.
-fn get_user_consent(verb: &Verb, cn: &str, privkeys: bool) -> bool {
-    if privkeys == true {
-        println!(
-            "{} certificates and private keys with common name matching: \"{}\". Okay? (y/n)",
-            verb, cn
-        );
-    } else {
-        println!(
-            "{} certificates with common name matching: \"{}\". Okay? (y/n)",
-            verb, cn
-        );
-    }
+fn get_user_consent(verb: &Verb) -> bool {
+    print!("{}; Okay? (y/n): ", verb);
+    io::stdout()
+        .flush()
+        .expect("Failed to flush stdout when printing confirmation message.");
 
     let mut input = String::new();
     io::stdin()
@@ -191,14 +163,16 @@ fn find_certs(path: PathBuf, cn: &str, privkeys: bool) -> ReplacePaths {
     let mut matched_keys = Vec::new();
     for cert in certs {
         if cert.common_name == cn {
-            match find_privkeys(&cert.cert, keys) {
-                Ok((matched, unmatched)) => {
-                    matched_keys.extend(matched);
-                    keys = unmatched;
-                }
-                Err((err, unmatched)) => {
-                    error!("Error on cert at {:?}: {:?}", cert.path, err);
-                    keys = unmatched;
+            if privkeys {
+                match find_privkeys(&cert.cert, keys) {
+                    Ok((matched, unmatched)) => {
+                        matched_keys.extend(matched);
+                        keys = unmatched;
+                    }
+                    Err((err, unmatched)) => {
+                        error!("Error on cert at {:?}: {:?}", cert.path, err);
+                        keys = unmatched;
+                    }
                 }
             }
             matched_certs.push(cert.path);
@@ -264,36 +238,16 @@ fn parse_pkiobjs(path: PathBuf) -> Result<Vec<PKIObject>, ParseError> {
         Ok(content) => {
             if let Some(key) = parse_privkey(&content) {
                 pkiobjs.push(PKIObject::PrivKey(PrivKey { key, path }));
-            } else if let Ok(cert) = X509::from_pem(&content) {
-                if let Some(common_name) = get_cn(&cert) {
-                    pkiobjs.push(PKIObject::Cert(Cert {
-                        cert,
-                        common_name,
-                        path,
-                    }));
-                }
-            } else if let Ok(cert) = X509::from_der(&content) {
-                if let Some(common_name) = get_cn(&cert) {
-                    pkiobjs.push(PKIObject::Cert(Cert {
-                        cert,
-                        common_name,
-                        path,
-                    }));
-                }
-            } else if let Ok(certs) = X509::stack_from_pem(&content) {
-                for cert in certs {
+            } else {
+                for cert in parse_cert(&content) {
                     if let Some(common_name) = get_cn(&cert) {
                         pkiobjs.push(PKIObject::Cert(Cert {
                             cert,
                             common_name,
                             path: path.clone(),
-                        }));
+                        }))
                     }
                 }
-            } else {
-                return Err(ParseError {
-                    msg: format!("Failed to parse data from file at: {:?}", path),
-                });
             }
         }
         Err(err) => {
---
+++ src/model.rs
@@ -0,0 +1,88 @@
+use openssl::pkey::{PKey, Private};
+use openssl::x509::X509;
+use std::fmt::Display;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub enum Verb {
+    Find {
+        cn: String,
+    },
+    Replace {
+        cn: String,
+        cert: Cert,
+        privkey: Option<PrivKey>,
+    },
+}
+
+impl Display for Verb {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            Self::Find { cn } => write!(f, "Finding certificates and associated private keys with common name matching: {}", cn),
+            Self::Replace {
+                cn,
+                cert: _,
+                privkey,
+            } => match privkey.is_some() {
+                true => write!(f, "Replacing certificates and associated private keys with common name matching: {}", cn),
+                false => write!(f, "Replacing certificates only with common name matching: {}", cn),
+            },
+        };
+    }
+}
+
+impl Verb {
+    /// Returns the target common name.
+    pub fn cn(&self) -> &str {
+        match self {
+            Self::Find { cn } => cn,
+            Self::Replace {
+                cn,
+                cert: _,
+                privkey: _,
+            } => cn,
+        }
+    }
+
+    /// Returns whether to also consider private keys.
+    pub fn privkeys(&self) -> bool {
+        match self {
+            Self::Find { cn: _ } => true,
+            Self::Replace {
+                cn: _,
+                cert: _,
+                privkey,
+            } => privkey.is_some(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+/// Models an X509 certificate.
+pub struct Cert {
+    pub cert: X509,
+    pub common_name: String,
+    pub path: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+/// Models an X509 certificate private key.
+pub struct PrivKey {
+    /// Private key.
+    pub key: PKey<Private>,
+    /// Path to file with key in.
+    pub path: PathBuf,
+}
+
+#[derive(Debug)]
+/// Holds paths of all the objects to be replaced.
+pub struct ReplacePaths {
+    pub certs: Vec<PathBuf>,
+    pub keys: Vec<PathBuf>,
+}
+
+#[derive(Debug)]
+pub enum PKIObject {
+    Cert(Cert),
+    PrivKey(PrivKey),
+}
