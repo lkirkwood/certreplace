3e2c6ff641aa90e177ae6263fa674d2ce48d53f8
Linus Kirkwood <linuskirkwood@gmail.com>
2022-11-24 19:19:05 +1100

    Switched to manual pem parsing for location in file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 mod model;
 
-use log::error;
+use log::{error, warn};
 use model::*;
 use openssl::nid::Nid;
 use openssl::pkey::{PKey, Private};
 use openssl::x509::X509;
+use std::collections::VecDeque;
 use std::fmt::Display;
 use std::path::PathBuf;
 use std::{
     fs,
     io::{self, Write},
+    str,
 };
 use structopt::StructOpt;
 
@@ -33,38 +35,16 @@ fn main() {
 
     let verb = match &args.certificate {
         Some(cert_path) => {
-            let mut x509s = parse_cert(
-                &fs::read(&cert_path).expect("Failed to read provided private key file."),
-            );
-            let x509 = match x509s.len() {
-                1 => x509s.pop().unwrap(),
-                other => panic!("Wrong number of certs found in provided file: {}", other),
-            };
-            let cn = match get_cn(&x509) {
-                None => match args.common_name {
-            None => panic!("No common name provided and failed to parse one from the provided certificate.."),
-                    Some(cn) => cn,
-                }
-                Some(cn) => cn
-            };
-            let cert = Cert {
-                cert: x509,
-                common_name: cn.clone(),
-                path: PathBuf::from(cert_path),
-            };
-
-            let privkey = match args.private_key {
+            let cert = choose_cert(cert_path, args.common_name.as_ref()).unwrap();
+            let privkey = match &args.private_key {
                 None => None,
-                Some(privkey_path) => {
-                    let content =
-                        fs::read(&privkey_path).expect("Failed to read provided private key file.");
-                    Some(PrivKey {
-                        key: parse_privkey(&content).unwrap(),
-                        path: PathBuf::from(privkey_path),
-                    })
-                }
+                Some(privkey_path) => Some(choose_privkey(privkey_path, &cert).unwrap()),
             };
-            Verb::Replace { cn, cert, privkey }
+            Verb::Replace {
+                cn: cert.common_name.clone(),
+                cert,
+                privkey,
+            }
         }
         None => match args.common_name {
             None => panic!("No certificate or common name provided."),
@@ -104,15 +84,13 @@ fn parse_privkey(content: &[u8]) -> Option<PKey<Private>> {
 }
 
 /// Parses a certificate from some bytes.
-fn parse_cert(content: &[u8]) -> Vec<X509> {
+fn parse_cert(content: &[u8]) -> Option<X509> {
     return if let Ok(cert) = X509::from_pem(&content) {
-        vec![cert]
+        Some(cert)
     } else if let Ok(cert) = X509::from_der(&content) {
-        vec![cert]
-    } else if let Ok(certs) = X509::stack_from_pem(&content) {
-        certs
+        Some(cert)
     } else {
-        vec![]
+        None
     };
 }
 
@@ -124,6 +102,88 @@ fn get_cn(cert: &X509) -> Option<String> {
         }
     }
     return None;
+}
+
+/// Chooses a certificate matching a common name from some pki objs,
+/// or returns an error if there is no unique match.
+fn choose_cert(path: &str, cn: Option<&String>) -> Result<Cert, ParseError> {
+    let path = PathBuf::from(path);
+    let pkis = parse_pkiobjs(PathBuf::from(path)).unwrap();
+
+    if cn.is_none() {
+        let mut certs = Vec::new();
+        for pki in pkis {
+            if let PKIObject::Cert(cert) = pki {
+                certs.push(cert);
+            }
+        }
+        if certs.len() == 1 {
+            return Ok(certs.pop().unwrap());
+        } else {
+            return Err(ParseError {
+                msg: "Certificate file does not contain exactly one certificate, so a common name must be provided.".to_string() 
+            });
+        }
+    } else {
+        let cn = cn.unwrap();
+
+        let mut certs = Vec::new();
+        for pki in pkis {
+            match pki {
+                PKIObject::Cert(cert) => {
+                    if &cert.common_name == cn {
+                        certs.push(cert);
+                    }
+                }
+                PKIObject::PrivKey(_) => {}
+            }
+        }
+        if certs.len() == 1 {
+            return Ok(certs.pop().unwrap());
+        } else {
+            return Err(ParseError {
+                msg: format!("Certificate file does not contain exactly one certificate with common name: {}", cn)
+            });
+        }
+    }
+}
+
+/// Chooses a private key matching a cert from some pki objs,
+/// or returns an error if there is no unique match.
+fn choose_privkey(path: &str, cert: &Cert) -> Result<PrivKey, ParseError> {
+    if let Ok(pubkey) = cert.cert.public_key() {
+        let path = PathBuf::from(path);
+        let pkis = parse_pkiobjs(PathBuf::from(path)).unwrap();
+        let mut privkeys = Vec::new();
+
+        for pki in pkis {
+            match pki {
+                PKIObject::PrivKey(pkey) => {
+                    if pkey.key.public_eq(&pubkey) {
+                        privkeys.push(pkey);
+                    }
+                }
+                PKIObject::Cert(_) => {}
+            }
+        }
+        if privkeys.len() == 1 {
+            return Ok(privkeys.pop().unwrap());
+        } else {
+            return Err(ParseError {
+                msg: format!(
+                "Provided file does not contain exactly one private key match cert with common name: {}",
+                cert.common_name
+            ),
+            });
+        }
+    } else {
+        return Err(ParseError {
+            msg: format!(
+                "Failed to get public key from provided certificate, cn: {}",
+                cert.common_name
+            ),
+        });
+    }
 }
 
 /// Returns true if user confirms operation.
@@ -145,15 +205,14 @@ fn get_user_consent(verb: &Verb) -> bool {
 fn find_certs(path: PathBuf, cn: &str, privkeys: bool) -> ReplacePaths {
     let mut certs = Vec::new();
     let mut keys = Vec::new();
-    for path in find_pkiobjs(path, privkeys) {
+    for path in find_pkiobj_files(path) {
         match parse_pkiobjs(path) {
-            Err(err) => error!("{err}"),
+            Err(err) => error!("{:?}", err),
             Ok(pkiobjs) => {
                 for pkiobj in pkiobjs {
                     match pkiobj {
                         PKIObject::Cert(cert) => {
                             if cert.common_name == cn {
-                                println!("{:?}", cert.cert.not_after());
                                 certs.push(cert);
                             }
                         }
@@ -169,28 +228,28 @@ fn find_certs(path: PathBuf, cn: &str, privkeys: bool) -> ReplacePaths {
     for cert in certs {
         if cert.common_name == cn {
             if privkeys {
-                match find_privkeys(&cert.cert, keys) {
+                match match_privkeys(&cert.cert, keys) {
                     Ok((matched, unmatched)) => {
                         matched_keys.extend(matched);
                         keys = unmatched;
                     }
                     Err((err, unmatched)) => {
-                        error!("Error on cert at {:?}: {:?}", cert.path, err);
+                        error!("Error on cert at {:?}: {:?}", cert.locator, err);
                         keys = unmatched;
                     }
                 }
             }
-            matched_certs.push(cert.path);
+            matched_certs.push(cert.locator);
         }
     }
 
     return ReplacePaths {
         certs: matched_certs,
-        keys: matched_keys.into_iter().map(|k| k.path).collect(),
+        keys: matched_keys.into_iter().map(|k| k.locator).collect(),
     };
 }
 
-fn find_pkiobjs(path: PathBuf, privkeys: bool) -> Vec<PathBuf> {
+fn find_pkiobj_files(path: PathBuf) -> Vec<PathBuf> {
     let mut paths = Vec::new();
     match fs::read_dir(&path) {
         Err(err) => error!("Failed while reading directory in {:?}: {:?}", path, err),
@@ -204,7 +263,7 @@ fn find_pkiobjs(path: PathBuf, privkeys: bool) -> Vec<PathBuf> {
                     if let Ok(ftype) = entry.file_type() {
                         if ftype.is_dir() {
                             // Recurse
-                            paths.extend(find_pkiobjs(entry.path(), privkeys));
+                            paths.extend(find_pkiobj_files(entry.path()));
                         } else {
                             if let Some((_, ext)) =
                                 entry.file_name().to_string_lossy().rsplit_once('.')
@@ -241,17 +300,30 @@ fn parse_pkiobjs(path: PathBuf) -> Result<Vec<PKIObject>, ParseError> {
     let mut pkiobjs = Vec::new();
     match fs::read(&path) {
         Ok(content) => {
-            if let Some(key) = parse_privkey(&content) {
-                pkiobjs.push(PKIObject::PrivKey(PrivKey { key, path }));
-            } else {
-                for cert in parse_cert(&content) {
+            for part in get_pem_parts(&content)? {
+                if let Some(privkey) = parse_privkey(part.data) {
+                    pkiobjs.push(PKIObject::PrivKey(PrivKey {
+                        key: privkey,
+                        locator: PEMLocator {
+                            path: path.clone(),
+                            start: part.start,
+                            end: part.start + part.data.len(),
+                        },
+                    }));
+                } else if let Some(cert) = parse_cert(part.data) {
                     if let Some(common_name) = get_cn(&cert) {
                         pkiobjs.push(PKIObject::Cert(Cert {
                             cert,
                             common_name,
-                            path: path.clone(),
-                        }))
+                            locator: PEMLocator {
+                                path: path.clone(),
+                                start: part.start,
+                                end: part.start + part.data.len(),
+                            },
+                        }));
                     }
+                } else {
+                    warn!("Failed to parse PKI object from PEM part: {:?}", part);
                 }
             }
         }
@@ -264,12 +336,68 @@ fn parse_pkiobjs(path: PathBuf) -> Result<Vec<PKIObject>, ParseError> {
             })
         }
     };
+    println!("{:#?}", pkiobjs);
     return Ok(pkiobjs);
+}
+
+const PEM_BOUNDARY: [char; 5] = ['-', '-', '-', '-', '-'];
+const PEM_BEGIN: [char; 5] = ['B', 'E', 'G', 'I', 'N'];
+const PEM_END: [char; 5] = ['-', 'E', 'N', 'D', ' '];
+
+/// Parses the data into PEM parts.
+fn get_pem_parts<'a>(data: &'a [u8]) -> Result<Vec<PEMPart<'a>>, ParseError> {
+    let string = match str::from_utf8(data) {
+        Ok(_string) => _string,
+        Err(err) => {
+            return Err(ParseError {
+                msg: format!("Failed to decode file data as utf-8: {:?}", err),
+            })
+        }
+    };
+    let mut parts = Vec::new();
+
+    let mut in_boundary = false;
+    let mut in_part = false;
+    let mut in_end = false;
+    let mut start = 0;
+
+    let mut index = 0;
+    let mut part_buf = Vec::new();
+    let mut buf = VecDeque::new();
+    for char in string.chars() {
+        index += 1;
+        buf.push_back(char);
+        if buf.len() > 5 {
+            buf.pop_front();
+        }
+
+        if in_part | in_boundary {
+            part_buf.push(char);
+        }
+
+        if buf == PEM_BOUNDARY {
+            in_boundary ^= true;
+            if in_end {
+                in_end = false;
+                parts.push(PEMPart {
+                    data: &data[start..index + 1],
+                    start,
+                })
+            }
+        } else if in_boundary & (buf == PEM_BEGIN) {
+            in_part = true;
+            start = index - 10;
+        } else if in_boundary & (buf == PEM_END) {
+            in_part = false;
+            in_end = true;
+        }
+    }
+    return Ok(parts);
 }
 
 /// Splits the keys into those that match the certificate (left) and those that dont (right).
 /// Upon error all keys are returned with the error.
-fn find_privkeys(
+fn match_privkeys(
     cert: &X509,
     keys: Vec<PrivKey>,
 ) -> Result<(Vec<PrivKey>, Vec<PrivKey>), (ParseError, Vec<PrivKey>)> {
@@ -307,7 +435,7 @@ fn print_paths(paths: ReplacePaths) {
     for cert in paths.certs {
         println!("\t{:#?}", cert);
     }
-    println!("Matching certificates:");
+    println!("Matching private keys:");
     for key in paths.keys {
         println!("\t{:#?}", key);
     }
